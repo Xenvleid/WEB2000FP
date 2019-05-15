a832d085d2ea796374c4c51ff734ebed3b5ab7f6
Xenvleid <45579399+Xenvleid@users.noreply.github.com>
2019-05-14 20:20:17 -0400

    Add files via upload

---
+++ zFinalProject/script.js
@@ -0,0 +1,42 @@
+var albums = [
+	{
+		id: 1,
+		title: "New Normals",
+		author: "Shaolin Dub",
+		desc: "Shaolin Dub is the dubstep, grime and reggae alter of London based producer Ketsa. For all your urban muic needs.",
+		picture: "https://freemusicarchive.org/file/images/artists/Shaolin_Dub_-_2018031594649051.jpg?width=290&height=290",
+		songs: [{
+			pos: 1, // position on the album
+			title: "Run Come",
+			size: "1.9", // megabytes
+			length: "3:53", // minutes
+			file: "http://freemusicarchive.org/music/Shaolin_Dub/New_Normals/Run_Come"
+        },
+        {
+			pos: 1, // position on the album
+			title: "Found",
+			size: "1.9", // megabytes
+			length: "3:02", // minutes
+			file: "http://freemusicarchive.org/music/Shaolin_Dub/New_Normals/Found"
+        },
+        {
+			pos: 1, // position on the album
+			title: "Bloody West",
+			size: "1.9", // megabytes
+			length: "3:13", // minutes
+			file: "http://freemusicarchive.org/music/Shaolin_Dub/New_Normals/Bloody_West"
+        },
+        {
+			pos: 1, // position on the album
+			title: "Destroyer",
+			size: "1.9", // megabytes
+			length: "4:00", // minutes
+			file: "http://freemusicarchive.org/music/Shaolin_Dub/New_Normals/Destroyer"
+        },
+        {
+			pos: 1, // position on the album
+			title: "Jive Town",
+			size: "1.9", // megabytes
+			length: "4:12", // minutes
+			file: "http://freemusicarchive.org/music/Shaolin_Dub/New_Normals/Jive_Town"
+        }
